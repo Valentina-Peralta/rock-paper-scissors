1d51f51981e46c2754b3c09f5844d53b7bb9dac0
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-08-14 18:24:57 -0300

    rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Board from './components/Board'
 import Header from './components/Header'
-
+import Rules from './components/Rules'
 function App() {
-
+  const [modal, setModal] = useState(false)
   return (
     <main className="app">
+      {modal &&
+        <Rules
+          onclick={
+            () => setModal(false)
+          } />
+      }
       <Header />
       <Board />
-      <button className='transparent_btn'>RULES</button>
+      <button className='transparent_btn'
+        onClick={() => setModal(true)}
+      >RULES</button>
     </main>
   )
 }
--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Rules = () => {
+import rules from '../images/modal.png'
+import '../stylesheets/Rules.css'
+const Rules = ({ onclick }) => {
     return (
-        <div className="rules">
-
+        <div className="modal">
+            <div className="rules">
+                <p><span>RULES</span>
+                    <span
+                        className='close'
+                        onClick={onclick}
+                    >X</span></p>
+                <img src={rules} alt="rules" />
+            </div>
         </div>)
 }
 
