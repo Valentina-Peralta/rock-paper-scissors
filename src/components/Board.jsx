39a1c3030e9216bdc8d916fdb5407e625312f1a3
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-08-14 17:25:02 -0300

    result message

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,49 +5,71 @@ const Board = () => {
     const options = ['rock', 'paper', 'scissors'];
     const [user, setUser] = useState('')
     const [computer, setComputer] = useState('')
+    const [counter, setCounter] = useState(0)
     const [userCounter, setUserCounter] = useState(0)
     const [computerCounter, setComputerCounter] = useState(0)
     const [result, setResult] = useState('')
+    const [score, setScore] = useState(0)
+
+    const reset = () => {
+        setUser('')
+        setComputer('')
+        setComputerCounter(0)
+        setUserCounter(0)
+        setResult('')
+        setCounter(0)
+    }
+
     const setWinner = () => {
+
         if (user === 'rock' && computer === 'scissors') {
             setUserCounter(prevUserCounter => prevUserCounter + 1);
         }
         if (user === 'rock' && computer === 'paper') {
             setComputerCounter(prevComputerCounter => prevComputerCounter + 1);
+
         }
         if (user === 'paper' && computer === 'scissors') {
             setComputerCounter(prevComputerCounter => prevComputerCounter + 1);
+
         }
         if (user === 'paper' && computer === 'rock') {
             setUserCounter(prevUserCounter => prevUserCounter + 1);
+
         }
 
         if (user === 'scissors' && computer === 'paper') {
             setUserCounter(prevUserCounter => prevUserCounter + 1);
+
         }
         if (user === 'scissors' && computer === 'rock') {
             setComputerCounter(prevComputerCounter => prevComputerCounter + 1);
-        }
-        if (user === computer) {
-            setComputerCounter(prevComputerCounter => prevComputerCounter + 1);
-            setUserCounter(prevUserCounter => prevUserCounter + 1);
 
         }
-        if (userCounter + computerCounter >= 3) {
-            userCounter > computerCounter ? setResult('YOU WIN') : setResult('YOU LOSE')
-        } else {
-            setTimeout(() => {
-                setUser('')
-                setComputer('')
-            }, 1500);
-        }
+
+
+
     }
 
     useEffect(() => {
         if (user && computer) {
             setWinner();
+            if (counter >= 3) {
+                if (userCounter > computerCounter) {
+                    setResult('YOU WIN');
+                    setScore(prevScore => prevScore + 1)
+                }
+                else { setResult('YOU LOSE') }
+            } else {
+                setTimeout(() => {
+                    setUser('')
+                    setComputer('')
+                }, 1500);
+            }
         }
-    }, [user, computer]);
+    }, [user, computer, counter]);
+
+    useEffect(() => { }, [counter])
 
     console.log(userCounter, computerCounter, user, computer)
 
@@ -58,13 +80,13 @@ const Board = () => {
 
     return (
         <>
-            {user === '' ?
+            {!user && !result ?
                 <div className="board">
                     <div className="paper"
                         onClick={() => {
                             setUser('paper')
                             getRandomOption()
-
+                            setCounter(prevCounter => prevCounter + 1)
                         }}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="49" height="59"><path fill="#3B4262" d="M47.125 11.832a2.922 2.922 0 00-1.232-.198c-.57.04-1.029.271-1.302.65-1.604 2.248-2.919 6.493-3.979 9.905-.486 1.577-1.14 3.688-1.612 4.69-.493-2.807.064-13.09.28-17.05l.003-.064c.15-2.751.17-3.234.138-3.446-.238-1.509-.843-2.5-1.799-2.943-.966-.45-2.22-.25-3.572.563-.677.41-.865 1.816-1.446 8.19l-.002.028c-.32 3.502-1.058 11.566-1.965 12.91-1.023-1.88-2.431-12.555-3.039-17.176-.425-3.236-.673-5.094-.84-5.655-.35-1.176-1.83-2.176-3.295-2.232-1.22-.06-2.22.56-2.698 1.638-.894.995-.578 4.292.41 12.102.47 3.718 1.44 11.395.83 12.257-1.219-.133-3.31-4.942-6.215-14.299-.816-2.62-1.068-3.408-1.318-3.753-.494-1.202-2.172-2.129-3.676-2.024a3.183 3.183 0 00-.377.049c-.787.156-2.584.881-2.2 4.226 1.06 4.637 2.213 8.041 3.331 11.346l.023.066c.669 1.98 1.302 3.85 1.89 5.925 1.385 4.9.846 7.94.84 7.975-.046.312-.143.503-.288.57a.556.556 0 01-.195.045c-.44.03-1.098-.26-1.437-.45-.776-1.482-4.636-8.544-8.134-9.524l-.126-.037-.127.012c-1.283.121-2.226.606-2.803 1.441-.914 1.32-.535 3.002-.444 3.34l.052.12c.028.051 2.834 5.165 3.268 7.544.374 2.04 2.311 4.25 3.869 6.026l.064.073c.508.58.946 1.083 1.292 1.548 4.519 4.713 11.665 8.677 11.723 8.71.892.657 1.387 1.293 1.44 1.84a.798.798 0 01-.16.58l-.155.162.988.96 18.853-1.324.804-3.684c2.486-10.402 1.967-19.272 1.958-19.33.01-.327.706-3.483 1.266-6.033l.017-.065c1.117-5.08 2.505-11.4 2.772-13.803.116-1.028-.542-1.972-1.675-2.401z" /></svg>
@@ -73,6 +95,7 @@ const Board = () => {
                         onClick={() => {
                             setUser('scissors')
                             getRandomOption()
+                            setCounter(prevCounter => prevCounter + 1)
 
 
                         }}
@@ -83,6 +106,7 @@ const Board = () => {
                         onClick={() => {
                             setUser('rock')
                             getRandomOption()
+                            setCounter(prevCounter => prevCounter + 1)
 
                         }}
                     >
@@ -119,12 +143,10 @@ const Board = () => {
                             <div className="result">
                                 <p>{result}</p>
                                 <button
-                                    onClick={() => {
-                                        setComputer('')
-                                        setUser('')
-                                    }}
-                                >PLAY AGAIN</button>
+                                    onClick={() => reset()
 
+                                    }
+                                >PLAY AGAIN</button>
                             </div>}
                         {computer === 'paper' ?
                             <div className="paper">
